5f5eb6d553d4df139ef2a8755610cb509b74dc23
HUANG-Stephanie <h810.stephanie@gmail.com>
2024-09-30 21:49:34 +0200

    performance + main

---
+++ Project1-automaton/KMP/Main.java
@@ -0,0 +1,30 @@
+package KMP;
+
+import java.util.List;
+import java.util.Scanner;
+
+import static KMP.KMP.extractContent;
+
+public class Main {
+    public static void main(String[] arg) {
+        String path;
+        String pattern;
+        if (arg.length > 1) {
+            path = arg[0];
+            pattern = arg[1].toLowerCase();
+        } else {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println(" >> Please enter the file's path: ");
+            path = scanner.next();
+            System.out.println(" >> Please enter the pattern: ");
+            pattern = scanner.next().toLowerCase();
+        }
+
+        StringBuilder text = extractContent(path);
+
+        KMP kmp = new KMP(pattern);
+        List<Integer> results = kmp.search(text.toString());
+        kmp.display();
+        System.out.println("Search CarryOver Results: " + results);
+    }
+}
