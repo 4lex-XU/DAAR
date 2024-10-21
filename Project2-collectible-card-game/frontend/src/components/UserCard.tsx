14c7cabb7c152e9a8771f4393043ec5704425b08
HUANG-Stephanie <h810.stephanie@gmail.com>
2024-10-21 09:33:44 +0200

    Front end - connexion smart contract

---
+++ Project2-collectible-card-game/frontend/src/components/UserCard.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { useEffect, useState } from 'react'
+import { WalletProps, Card } from '../apiPokeTCG/types';
+
+const UserCard: React.FC<WalletProps> = ({wallet}) => {
+    const [cards, setCardsValue] = useState<Card[]>([])
+  
+    useEffect(() => {
+      if (wallet?.contract && wallet?.details.account) {
+        getCardsByOwner();
+      }
+    },[wallet])
+  
+    async function getCardsByOwner() {
+        if (wallet?.contract && wallet?.details.account) {
+            try {
+            const data = await wallet.contract.getCardsByOwner(wallet.details.account)
+            const formattedCards = data.map((tuple: [number, string]) => ({
+                num: tuple[0],
+                img: tuple[1]
+            }));
+            setCardsValue(formattedCards);
+            console.log('getCardsByOwner: '+wallet.details.account+' reussie !')
+            } catch (err) {
+            console.log('Erreur lors du getCardsByOwner.')
+            console.error(err)
+            }
+        }
+    }
+
+    return (
+        <div>
+        <h1>Mes Cartes</h1>
+        <ul>
+            {cards.length > 0 ? (
+            cards.map((card, index) => (
+                <li key={index}>
+                Carte #{card.num}: {card.img}
+                </li>
+            ))
+            ) : (
+                <li>Aucune carte trouvée</li>
+            )}
+        </ul>
+        </div>
+    );
+}
+
+export default UserCard;
